3863105298ecba5fa349ba400776f933a359585f
amresh <iamresh8@gmail.com>
2016-08-19 18:16:27 +0530

    test commit123

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-console.log("Hello World!");
+console.log("Hello World!!");
 var f = function(a,b,c,d){
-return a+b+c+d;
+return a+b+c*d;
 };
-console.log(f(2,3,4,5));
+console.log(f(2,3,4,5))
+console.log(f(2,3,4,1))
 
